f9784e2d9e6cd5465926a6c32709f727738e12d1
Piyush Raj <sciencetechoindia@gmail.com>
2026-02-08 18:23:42 +0530

    feat(answer): add POST and DELETE endpoints for answer management with reputation updates

---
+++ app/api/vote/route.ts
@@ -0,0 +1,109 @@
+import { answerCollection, db, questionCollection, voteCollection } from "@/models/name";
+import { databases, users } from "@/models/server/config";
+import { UserPrefs } from "@/store/auth";
+import { NextRequest, NextResponse } from "next/server";
+import { ID, Query } from "node-appwrite";
+
+export async function POST(request: NextRequest) {
+    try {
+        const {type, typeId, voteStatus, voteById} = await request.json()
+
+        const response = await databases.listDocuments(db, voteCollection, [
+            Query.equal("type", type), Query.equal("typeId", typeId), Query.equal("voteById", voteById)
+        ])
+
+        if (response.documents.length > 0) {
+            await databases.deleteDocument(db, voteCollection, response.documents[0].$id)
+
+            const questionOrAnswer = await databases.getDocument(
+                db, type === "question" ? questionCollection : answerCollection, typeId
+            )
+
+            const authorPrefs = await users.getPrefs<UserPrefs>(questionOrAnswer.authorId)
+
+            await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+                reputation:
+                response.documents[0].voteStatus === "upvoted" ? Number(authorPrefs.reputation - 1) : Number(authorPrefs.reputation + 1)
+            })
+        }
+
+        if (response.documents[0]?.voteStatus !== voteStatus) {
+            const doc = await databases.createDocument(db, voteCollection, ID.unique(), {
+                type, typeId, voteStatus, voteById
+            })
+
+            const questionOrAnswer = await databases.getDocument(
+                db, type === "question" ? questionCollection : answerCollection, typeId
+            )
+
+            const authorPrefs = await users.getPrefs<UserPrefs>(questionOrAnswer.authorId)
+
+            if (response.documents[0]) {
+                await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+                    reputation:
+                    response.documents[0].voteStatus === "upvoted" ? Number(authorPrefs.reputation - 1) : Number(authorPrefs.reputation + 1)
+                })
+            } else {
+                await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+                    reputation:
+                    voteStatus === "upvoted" ? Number(authorPrefs.reputation + 1) : Number(authorPrefs.reputation - 1)
+                })
+            }
+
+            const [upvotes, downvotes] = await Promise.all([
+                databases.listDocuments(db, voteCollection, [
+                    Query.equal("type", type),
+                    Query.equal("typeId", typeId),
+                    Query.equal("voteStatus", "upvoted"),
+                    Query.equal("voteById", voteById),
+                    Query.limit(1)
+                ]),
+                databases.listDocuments(db, voteCollection, [
+                    Query.equal("type", type),
+                    Query.equal("typeId", typeId),
+                    Query.equal("voteStatus", "downvoted"),
+                    Query.equal("voteById", voteById),
+                    Query.limit(1)
+                ]),
+            ])
+    
+            return NextResponse.json(
+                {
+                    data: {document: doc, voteResult: upvotes.total - downvotes.total},
+                    message: response.documents[0] ? "Vote status updated" : "Voted"
+                },
+                {status: 201}
+            )
+        }
+        
+        const [upvotes, downvotes] = await Promise.all([
+            databases.listDocuments(db, voteCollection, [
+                Query.equal("type", type),
+                Query.equal("typeId", typeId),
+                Query.equal("voteStatus", "upvoted"),
+                Query.equal("voteById", voteById),
+                Query.limit(1)
+            ]),
+            databases.listDocuments(db, voteCollection, [
+                Query.equal("type", type),
+                Query.equal("typeId", typeId),
+                Query.equal("voteStatus", "downvoted"),
+                Query.equal("voteById", voteById),
+                Query.limit(1)
+            ]),
+        ])
+
+        return NextResponse.json(
+            {
+                data: {document: null, voteResult: upvotes.total - downvotes.total},
+                message: "Vote Withdrawn"
+            },
+            {status: 200}
+        )
+    } catch (error: any) {
+        return NextResponse.json(
+            {error: error?.message || "Error in voting"},
+            {status: error?.status || error?.code || 500}
+        )
+    }
+}
